65b2023438fe97e0c392774d114d2fc1a4f75085
liamasis <liamcalebob@gmail.com>
2023-11-30 23:30:44 +0000

    Starting the Django backend

---
+++ backendapi/bestbloadapp/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import *
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+class UserRegSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=100, write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data['username'],
+            password = validated_data('password'),
+        )
+
+        return user
---
+++ backendapi/bestbloadapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+from .models import *
+from django.contrib.auth.models import User
+
+
+@api_view(['GET'])
+def home(request):
+    posts = Post.objects.all()
+    serialize = PostSerializer(posts, many=True)
+    return Response(serialize.data)
+
+@api_view(['GET'])
+def category(request):
+    cat = Category.objects.all()
+    seralize = CategorySerializer(cat, many=True)
+    return Response(seralize.data)
+
+@api_view(['GET'])
+def users(request):
+    user = User.objects.all()
+    serialise = UserSerializer(user, many=True)
+    return Response(serialise.data)
+
+@api_view(['POST'])
+def user_registration(request):
+    serializer = UserRegSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
