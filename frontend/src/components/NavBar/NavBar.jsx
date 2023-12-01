ad34c314b9002d1eac9bdf5cefe4c83d8c745033
liamasis <liamcalebob@gmail.com>
2023-12-01 19:16:59 +0000

    css styling on nav

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -4,22 +4,30 @@ export const NavBar = () => {
   return (
     <div className="top_section">
       <nav className='nav_wrapper'>
-        <div>
-            What is best
+        <div className='logo'>
+            BEST
         </div>
 
         <div>
           <ul className='menu_wrapper'>
-            <li>All</li>
-            <li>Technology</li>
-            <li>Food</li>
-            <li>Hostels</li>
-            <li>Climate</li>
-            <li>Sites</li>
-            <li>Underground</li>
+            <li className='nav_link'>All</li>
+            <li className='nav_link'>Technology</li>
+            <li className='nav_link'>Food</li>
+            <li className='nav_link'>Hostels</li>
+            <li className='nav_link'>Climate</li>
+            <li className='nav_link'>Sites</li>
+            <li className='nav_link'>Underground</li>
           </ul>
         </div>
+        <div>
+          <div className="button_holder">
+            <button>Login</button>
+            <button>Register</button>
+          </div>
+        </div>
       </nav>
+      
+
     </div>
   )
 }
