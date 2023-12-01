c044f43b96f754fbba32cd02dbdaf5981e119cbb
liamasis <liamcalebob@gmail.com>
2023-12-01 12:02:24 +0000

    Rename project

--- backendapi/bestblogapp/migrations/0001_initial.py
+++ backendapi/bestblogapp/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
                 ('descripts', models.TextField()),
                 ('image', models.ImageField(upload_to='images/')),
                 ('date_added', models.DateTimeField(auto_now_add=True)),
-                ('category', models.ManyToManyField(to='bestbloadapp.category')),
+                ('category', models.ManyToManyField(to='bestblogapp.category')),
                 ('owner', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
