41427dedcd708f5923957930ade07f2afbb7b0ac
liamasis <liamcalebob@gmail.com>
2023-12-02 15:24:47 +0000

    adding material ui and building some lorem content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import { NavBar } from './components/NavBar/NavBar'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Home from './components/Home/Home'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './App.scss';
+
 import {
   BrowserRouter as Router,
   Routes,
--- frontend/src/components/SidePanel/SidePanel.jsx
+++ frontend/src/components/SidePanel/SidePanel.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 
 export const SidePanel = () => {
   return (
-    <div className='container'>SidePanel</div>
+    <div className='container'>
+      <div className="">
+        <h4>
+          Visting the best places around the world.
+        </h4>
+        <div className="">
+          <div className="">
+            <p>
+              First thing
+            </p>
+          </div>
+        </div>
+        <div className="">
+          <div className="">
+            <p>
+              First thing
+            </p>
+          </div>
+        </div>
+        <div className="">
+          <div className="">
+            <p>
+              First thing
+            </p>
+          </div>
+        </div>
+        <div className="">
+          <div className="">
+            <p>
+              First thing
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
