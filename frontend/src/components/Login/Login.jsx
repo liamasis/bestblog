09c698e33cea885a2cb4b873fc1f4ab0e589cb83
liamasis <liamcalebob@gmail.com>
2023-12-18 19:35:16 +0000

    build: login screen

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
-import Counter from '../../features/counter/Counter.jsx'
+import { useState } from 'react'
+import './Login.scss'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [check, setCheck] = useState(false)
+
+  const [errMsg, setErrMsg] = useState('')
+
+  const navigate = useNavigate()
+  const login_container = useLocation()
+
+  const dispatch = useDispatch()
+
+  const handleUsernameInput = (e) => setUsername(e.target.value)
+  const handlePasswordInput = (e) => setPassword(e.target.value)
+
+  const togglePasswordVisibility = () => {
+    setCheck((prevState) => !prevState)
+  }
   return (
-    <div>
-        <Counter />
+    <div className="login_container">
+      <div className="login_box">
+        <h3 className="login_heading">LOGIN</h3>
+        <div className="something"></div>
+        <form className="login_form" action="">
+          <label>Username</label>
+          <input
+            type="text"
+            required
+            name="username"
+            className="input_username"
+          />
+          <label>Password</label>
+          <input
+            type="text"
+            required
+            name="password"
+            className="input_password"
+          />
+          <div className="">
+            <label htmlFor="">show password</label>
+            <input type="checkbox" name="show_password" id="" />
+          </div>
+          <button className="login_button">Login</button>
+        </form>
+        <p>
+          Don't have an account?
+          <Link className="register_link" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
